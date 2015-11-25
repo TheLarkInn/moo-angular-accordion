93f6b8615c3ba05664c5f9fc4b8549aa2c94ed0b
Sean Larkin <sean.larkin@mutualofomaha.com>
2015-11-25 12:18:11 -0600

    fixed module definition to UMD

--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import mooAccordionDirective from './mooAccordion.js';
 (function (root, factory) {
   'use strict';
   if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
     define(['angular'], factory);
-  } else if (typeof module !== 'undefined' && typeof module.exports === 'object') {
-    // CommonJS support (for us webpack/browserify/ComponentJS folks)
+  } else if (root.hasOwnProperty('angular')) {
+    // Browser globals (root is window), we don't register it.
+    factory(root.angular);
+  } else if (typeof exports === 'object') {
     module.exports = factory(require('angular'));
-  } else {
-    // in the case of no module loading system
-    // then don't worry about creating a global
-    // variable like you would in normal UMD.
-    // It's not really helpful... Just call your factory
-    return factory(root.angular);
   }
 }(this, function (angular) {
   'use strict';
